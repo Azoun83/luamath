6c476ec8a227ce676609daf5e34f268c48ade636
Azoun83 <asuka_michele@yahoo.it>
2024-09-22 11:32:53 +0200

    Delete luamath_minimal.h

--- luamath_minimal.h
+++
@@ -1,8 +0,0 @@
-#include "luamath_minimal_fun.h"
-
-
-#define LUAMATH_METATABLE "luamathminimal"
-
-static int lm_new(lua_State *L);
-static int lm_identity(lua_State *L);
-static int lm_print(lua_State *L);
